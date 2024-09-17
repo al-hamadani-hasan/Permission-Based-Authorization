ad183f60e6e7b9b12a1bd1b6c5aed3754158c349
al-hamadani-hasan <mr.alhamadani.hasan@gmail.com>
2024-09-17 08:46:16 +0300

    Initialize project

---
+++ Permission-Based-Authorization/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Permission_Based_Authorization.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
