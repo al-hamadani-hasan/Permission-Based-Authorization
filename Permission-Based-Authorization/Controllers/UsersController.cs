54258e60ae42b7209f5b89e9adecf41a01f16dc8
al-hamadani-hasan <mr.alhamadani.hasan@gmail.com>
2024-09-18 10:17:02 +0300

    Create a users controller and adding index page

---
+++ Permission-Based-Authorization/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Permission_Based_Authorization.Models;
+using Permission_Based_Authorization.Repositories;
+using System.Xml;
+using X.PagedList.Extensions;
+
+namespace Permission_Based_Authorization.Controllers
+{
+    [Route("user")]
+    [Authorize(Roles = "Administrator")]
+    public class UsersController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(
+            IConfiguration configuration,
+            IUserRepository userRepository)
+        {
+            _configuration = configuration;
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Index(int? page = 1)
+        {
+            int pageSize = _configuration.GetValue<int>("PaginationSettings:PageSize");
+
+            var user = await _userRepository.CurrentUserAsync(HttpContext);
+
+            var usersExceptResult = await _userRepository.ExceptUserByIdAsync(user.Id);
+            var users = usersExceptResult.ToPagedList(page ?? 1, pageSize);
+
+            return View(users);
+        }
+
+    }
+}
